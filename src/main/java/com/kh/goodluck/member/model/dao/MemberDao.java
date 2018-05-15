22ad8c7dc73a742030c7bdb3b1c3b17aca615233
LeeByungJun <semicolon92@naver.com>
2018-05-15 14:32:26 +0900

    lbj - 5월15일 커밋

--- src/main/java/com/kh/goodluck/member/model/dao/MemberDao.java
+++ src/main/java/com/kh/goodluck/member/model/dao/MemberDao.java
@@ -20,6 +20,16 @@ public class MemberDao {
 		//로그인용 메소드
 		return sqlSession.selectOne("loginCheck", member);
 	}
+
+	public int findPwdMethod(Member member) {
+		//비밀번호 재설정용 메소드
+		return sqlSession.update("fwdChange", member);
+	}
+
+	public Member emailCheckMethod(String tomail) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("emailCheck",tomail);
+	}
 	
 	
 }
--- src/main/java/com/kh/goodluck/member/model/service/MemberService.java
+++ src/main/java/com/kh/goodluck/member/model/service/MemberService.java
@@ -4,4 +4,6 @@ import com.kh.goodluck.member.model.vo.Member;
 
 public interface MemberService {
 	public abstract Member loginCheck(Member member);
+	public abstract int findPwdMethod(Member member);
+	public abstract Member emailCheckMethod(String tomail);
 }
