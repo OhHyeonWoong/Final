6b15fc894edd72deeeea8d072e451b9950d294fb
Choijiseok1 <gkffhqnd@naver.com>
2018-05-20 19:40:11 +0900

    ㄴ

--- src/main/java/com/kh/goodluck/item/controller/ItemController.java
+++ src/main/java/com/kh/goodluck/item/controller/ItemController.java
@@ -19,8 +19,11 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.goodluck.item.model.service.ItemService;
+import com.kh.goodluck.item.model.vo.GetMyItem;
 import com.kh.goodluck.item.model.vo.ITEMLIST;
 import com.kh.goodluck.member.model.vo.Member;
+
+import jdk.incubator.http.HttpResponse;
 
 @Controller
 public class ItemController {
@@ -46,13 +49,20 @@ public class ItemController {
 	
 	@RequestMapping("cjsitemmellhome.go")
 	public ModelAndView itemmallmove(ModelAndView mv) {
-	//아이템몰 메인	
+		
+		//아이템몰 메인	
 		
 		//8개 뽑기. 최신.
 		ArrayList<ITEMLIST> al= (ArrayList<ITEMLIST>)ItemService.homenewlitm();
 	
 		//4개뽑기 인기
 		ArrayList<ITEMLIST> al1= (ArrayList<ITEMLIST>)ItemService.homepopularlitm();
+		
+		//카로셀에 넣을것.
+		
+		//공지사항
+		
+		//할인 목록.
 		
 		
 		mv.addObject("newitem",al);
@@ -71,6 +81,22 @@ public class ItemController {
 		return mv;
 	}
 
+	@RequestMapping("cjsgetmyitem.go")
+	public void getmyitem(@RequestParam("member_id") String memberid, HttpServletResponse response) throws IOException {
+	
+		System.out.println("member : " + memberid);
+		//로그인 작업을 합니다 세션에 넣어요
+		
+		GetMyItem gmi = new GetMyItem();
+		
+		PrintWriter out = response.getWriter();
+
+		out.flush();
+		out.close();
+	
+	}
+	 
+	
 }
 
 
