3fb80f591a75e0d810f9bd378fa210a83bcd7348
LeeByungJun <semicolon92@naver.com>
2018-05-24 15:06:05 +0900

    lbj - myPage 에서 QNA 작업중

--- src/main/java/com/kh/goodluck/qna/model/service/QNAServiceImpl.java
+++ src/main/java/com/kh/goodluck/qna/model/service/QNAServiceImpl.java
@@ -1,5 +1,32 @@
 package com.kh.goodluck.qna.model.service;
 
-public class QNAServiceImpl implements QNAService{
+import java.util.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.goodluck.qna.model.dao.QNADao;
+import com.kh.goodluck.qna.model.vo.QNA;
+import com.kh.goodluck.qna.model.vo.QnaAnswer;
+
+@Service("qnaService")
+public class QNAServiceImpl implements QNAService{
+	
+	@Autowired
+	private QNADao qnaDao; 
+
+	@Override
+	public List<QNA> selectMyQna(String member_id) {
+		return qnaDao.selectMyQna(member_id);
+	}
+	
+	@Override
+	public QNA selectMyDetailQna(QNA q) {
+		return qnaDao.selectMyDetailQna(q);
+	}
+	
+	@Override
+	public QnaAnswer selectMyDetailQnaAnswer(int question_no) {
+		return qnaDao.selectMyDetailQnaAnswer(question_no);
+	}
 }
